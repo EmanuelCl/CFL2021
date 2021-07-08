3cd255b0c6f70d20f2967ca5e610e0a0142ac510
EmanuelCL <emiclementi@hotmail.com>
2021-07-07 21:09:12 -0300

    ejercicio2

--- Programacion Orientada a Objetos/clase-ts/ejercicio2/ejercicio2.ts
+++ Programacion Orientada a Objetos/clase-ts/ejercicio2/ejercicio2.ts
@@ -2,25 +2,50 @@ import * as readlineSync from "readline-sync"
 
 let cantidad:number=readlineSync.question("Cuantas palabras desea Ingresar? ");
 let arreglo:string[]=[]
+
 function cargarPalabras(arreglo:string[],cantidad:number):void{
-    for(let i:number=0;i<cantidad;i++){
-        arreglo[i]=readlineSync.question("Ingrese palabras: ")
+    for(let indice:number=0;indice<cantidad;indice++){
+        arreglo[indice]=readlineSync.question("Ingrese palabras: ")
     }
 }
-cargarPalabras(arreglo,cantidad)
+cargarPalabras(arreglo,cantidad);
 console.log(arreglo);
+opciones(arreglo);
 
-function modificar (arreglo:string[]):void {
-    let palabra:number=readlineSync.question("Indique la posicion en la cual desea Insertar/eliminar/buscar/actualizar palabras: ");
+function opciones(arreglo:string[]){
+    let pregunta=readlineSync.question("Elija un numero para realizar la siguiente accion: (1=Insertar) (2=eliminar) (3=buscar) (4=actualizar) : ");
+    if(pregunta===1){
+        insertar(arreglo)
+    }else if(pregunta===2){
+        eliminar(arreglo)
+    }else if(pregunta===3){
+        buscar(arreglo)
+    }else if(pregunta===4){
+        actualizar(arreglo)
+    }
+}
+
+function actualizar (arreglo:string[]):void {
+    let palabra:number=readlineSync.question("Indique la posicion de la palabra que desea actualizar: ");
     console.log(arreglo[palabra])
-    arreglo[palabra]=readlineSync.question("Inserte/elimine/busque/actualice palabras: ");
+    arreglo[palabra]=readlineSync.question("Ingrese la modificacion: ");
     console.log(arreglo);
 }
-modificar(arreglo);
 
-//Ejercicio 2 (Partir de cero)
-//Definir funciones (con todos los tipos necesarios) para
-//hacer lo siguiente:
-//Cargar un listado de palabras (por esta vez, usar el arreglo
-//como variable global)
-//Insertar/eliminar/buscar/actualizar una palabra del listado
+function insertar (arreglo:string[]):void {
+    let palabra:string=readlineSync.question("Ingrese la palabra que sea agregar: ");
+    arreglo.push(palabra)
+    console.log(arreglo)
+}
+
+function buscar (arreglo:string[]):void {
+    let palabra:number=readlineSync.question("Indique la posicion de la palabra que desea buscar: ");
+    console.log(arreglo[palabra]);
+    console.log(arreglo);
+}
+
+function eliminar (arreglo:string[]):void {
+    let palabra:number=readlineSync.question("Ingrese la posicion de la palabra que desea eliminar: ");
+    arreglo.splice(palabra)
+    console.log(arreglo);
+}
